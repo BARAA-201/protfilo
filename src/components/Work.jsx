49985e1f90a4c135b36d26217cc734d5e510d996
BARAA-201 <albr570@gmail.com>
2022-10-21 22:03:17 +0300

    add alasadi proj card

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -21,10 +21,28 @@ function Work() {
 
                         <div className='opacity-0 group-hover:opacity-100 duration-500 '>
                             <span className='text-2xl font-[ArsenicaExtrabold] text-[#042D30] tracking-wider flex text-center '>
-                                React and Node Application
+                                EASY REACH
                             </span>
                             <div className='pt-8 text-center'>
                                 <a href="https://easy-reach.herokuapp.com/home" target="_blank">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-zinc-200 text-gray-700 font-bold text-lg'>Visit Website</button>
+                                </a>
+                                <a href="/" target="_blank">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-zinc-200 text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div style={{ backgroundImage: `url(${'https://firebasestorage.googleapis.com/v0/b/myportfoilo-1108f.appspot.com/o/alasadi.jpg?alt=media&token=1d0dbad8-fabc-4861-8ed0-fe457263abd8'})` }} className='shadow-lg shadow-[#003034] group container rounded-md flex justify-center items-center mx-auto content-div '>
+
+                        <div className='opacity-0 group-hover:opacity-100  duration-500'>
+                            <span className='text-2xl font-[ArsenicaExtrabold] text-[#042D30] tracking-wider flex justify-center text-center'>
+                                ALASDI ART
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="https://www.alasadiart.com/" target="_blank">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-zinc-200 text-gray-700 font-bold text-lg'>Visit Website</button>
                                 </a>
                                 <a href="/" target="_blank">
